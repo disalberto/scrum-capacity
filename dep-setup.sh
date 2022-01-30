d9882fb0c2ee60f5e45d6c40483ed6affb29d4fc
Alberto DI SAVIA PUGLISI <alberto.disaviapuglisi@amadeus.com>
2022-01-30 14:56:02 +0100

    fix pylint setup

--- dep-setup.sh
+++ dep-setup.sh
@@ -2,9 +2,13 @@
 
 PYVER="$(echo $(python -V) | sed 's/Python \(.*\..*\)\..*/\1/')"
 
+sudo apt-get update
+
 sudo apt-get install -y "python${PYVER}-dev" "libpython${PYVER}-dev" 
 #TODO check if they exist before
 
-sudo apt-get install -y dpkg-dev build-essential freeglut3-dev libgl1-mesa-dev libglu1-mesa-dev libgstreamer-plugins-base1.0-dev libgtk-3-dev libjpeg-dev libnotify-dev libpng-dev libsdl2-dev libsm-dev libtiff-dev libwebkit2gtk-4.0-dev libxtst-dev
+#sudo apt-get install -y dpkg-dev build-essential freeglut3-dev libgl1-mesa-dev libglu1-mesa-dev libgstreamer-plugins-base1.0-dev libgtk-3-dev libjpeg-dev libnotify-dev libpng-dev libsdl2-dev libsm-dev libtiff-dev libwebkit2gtk-4.0 libxtst-dev
+
+sudo apt install build-essential python3-pip make gcc libgtk-3-dev libgstreamer-gl1.0-0 freeglut3 freeglut3-dev python3-gst-1.0 libglib2.0-dev ubuntu-restricted-extras libgstreamer-plugins-base1.0-dev
 
 pip install -r requirements.txt
